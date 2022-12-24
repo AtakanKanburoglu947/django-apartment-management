a33ad57b6805f69b5ea25594f8319295936e3abb
Atakan <88630930+Saloli88@users.noreply.github.com>
2022-12-24 22:48:21 +0300

    Views ve Frontend Güncellendi

--- apartman_yonetimi/apartman_yonetimi/core/views.py
+++ apartman_yonetimi/apartman_yonetimi/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.password_validation import validate_password
 from django.contrib import messages
 from core.models import Profile,Content,Image,Comment,Message,Faq,Payment,Menu,Request,Setting
 from django.http import HttpResponse
@@ -121,8 +121,7 @@ def signin(request):
             auth.login(request,user)
             return redirect('/')
         else:
-            messages.info(request,'Credentials Invalid')
-            return redirect('signin')
+            messages.info(request,'Hesap bulunamadı ')
     else:
         return render(request,'signin.html')
 @login_required(login_url='signin')
@@ -162,15 +161,11 @@ def signup(request):
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
-        password2 = request.POST['password2']
-        if password == password2:
-            if User.objects.filter(email=email).exists():
-                messages.info(request,'Email taken')
-                return redirect('signup')
-            elif User.objects.filter(username=username).exists():
-                messages.info(request,'Username taken')
-                return redirect('signup')
-            else:
+        if User.objects.filter(email=email).exists():
+            messages.info(request,'E-posta alınmış')
+        elif User.objects.filter(username=username).exists():
+            messages.info(request,'Kullanıcı adı alınmış')
+        else:
                user = User.objects.create_user(username=username,email=email,password=password)
                user.save()
                user_login = auth.authenticate(username=username,password=password)
@@ -179,8 +174,5 @@ def signup(request):
                new_profile = Profile.objects.create(user=user_model,id_user=user_model.id)
                new_profile.save()
                return redirect('settings')
-        else:
-            messages.info(request,'Password does not match')
-            return
     else:
         return render(request,'signup.html')
